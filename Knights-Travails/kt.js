aa5d482767c0891c56b267829a56261b7d2c96b0
Philip Pretorius <philippretorius.79@gmail.com>
2023-09-16 00:17:27 +0200

    Cell class added

--- Knights-Travails/kt.js
+++ Knights-Travails/kt.js
@@ -1,20 +1,23 @@
-
-const gameBoard = () => {
-  let chessBoard = [];
-  let cell = 0;
-  for (let r = 0; r < 8; r++) {
-    for (let c = 0; c < 8; c++) {
-      chessBoard[cell] = [r, c];
-      cell++;
-    }
+class Cell {
+  constructor(x, y, distance) {
+    this.x = x;
+    this.y = y;
+    this.distance = distance;
   }
 }
 
-
-function validMove(r, c) {
-  return (r < 8 && r >= 0 && c < 8 && c >= 0 ) ? true : false;
+function validMove(x, y) {
+  return (x < 8 && x >= 0 && y < 8 && y >= 0 ) ? true : false;
 }
 
 function knightMoves(position, target) {
+
+  // Different move combinations
+  let dx = [-2, -1, 1, 2, -2, -1, 1, 2];
+  let dy = [-1, -2, -2, -1, 1, 2, 2, 1];
+
+  let q = [];
+  // Starting position of knight
+  q.push(new Cell(knightPos[0], knightPos[1], 0));
 
 }
